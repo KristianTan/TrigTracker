76e02f130ed70ee25c0f9652e445bdefdc83ca3c
kristian <kristian.tan@ntlworld.com>
2019-08-14 21:50:02 +0100

    Read csv file

--- app/src/main/java/com/uk/trigtracker/FirstFragment.java
+++ app/src/main/java/com/uk/trigtracker/FirstFragment.java
@@ -3,6 +3,7 @@ package com.uk.trigtracker;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class FirstFragment extends Fragment implements OnMapReadyCallback {
 
@@ -58,6 +69,28 @@ public class FirstFragment extends Fragment implements OnMapReadyCallback {
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         LatLng marker = new LatLng(53.3499986, -1.83333);
+
+        InputStream is = getResources().openRawResource(R.raw.peaks_trigs);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                is,
+                Charset.forName("UTF-8")));
+
+        ArrayList<String[]> lines = new ArrayList<>();
+        String line;
+        try {
+
+            // Step over headers
+            reader.readLine();
+
+            while((line = reader.readLine()) != null) {
+                String[] tokens = line.split(",");
+                lines.add(tokens);
+            }
+            System.out.println(lines);
+        } catch (IOException e) {
+            Log.wtf("MainActivity", "Error reading file at line 31: " + e);
+            e.printStackTrace();
+        }
 
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker, 9.5f));
     }
